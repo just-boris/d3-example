49619d9e702b1f662ae3fcf0aa1eaaf24c2091c0
Boris <just-boris@hotmail.com>
2012-11-21 23:31:13 +0400

    рисование полилинии комненсация 2gb на нижнем графике

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
+"use strict";
+var WIDTH = 500,
+    PULSE_WIDTH = 10,
+    RESET_VALUE = 200,
+    INCREMENT = 10,
+    DELTA = 60;
 window.addEventListener('load', function() {
-var subtract = function(a, b) {
+var
+    subtract = function(a, b) {
         var result = [];
         for(var i = 0; i < Math.min(a.length, b.length); i++) {
             result.push(a[i] - b[i]);
@@ -8,8 +15,8 @@ var subtract = function(a, b) {
     },
     data1 = (function getData() {
         var res = [];
-        for(var i = 0; i < 500/10; i++) {
-            res.push((i*10 + 60 * (i % 2)) % 200)
+        for(var i = 0; i < WIDTH/PULSE_WIDTH; i++) {
+            res.push((i*INCREMENT  + DELTA * ((i+1) % 2)) % RESET_VALUE)
         }
         return res;
     })(),
@@ -19,7 +26,7 @@ var subtract = function(a, b) {
         res.pop();
         return res;
     })(data1),
-    difference = subtract(data1, data2).map(function(i) {return i+130}),
+    difference = subtract(data1, data2).map(function(i) {return (i+200)%200}),
     g1 = new SawGraph('#graph1', 'steelblue'),
     g2 = new SawGraph('#graph2', 'green'),
     g3 = new SawGraph('#graph3', 'yellow');
@@ -30,26 +37,33 @@ var subtract = function(a, b) {
 });
 
 function SawGraph(selector, color) {
-    var container = d3.select(selector);
+    var me = this,
+        container = d3.select(selector);
     this.svg =  container.append('svg');
     this.color = color;
     this.width = container.node().offsetWidth;
     this.height = container.node().offsetHeight;
+    this.x = function(x) {return x;};
+    this.y = function(y) {return me.height - y};
+
+    this.hline = d3.svg.line().x(function(d, i) {
+        return me.x(PULSE_WIDTH*i);
+    }).y(function() {
+            return me.y(me.height/2);
+    }).interpolate("linear");
+    this.line = function(data) {
+        var result = '';
+        data.forEach(function(d, i) {
+            result += (i===0?'M'+me.x(PULSE_WIDTH*i)+','+me.y(d):'V'+me.y(d))+'H'+me.x(PULSE_WIDTH*(i+1));
+        });
+        return result;
+    };
 }
 SawGraph.prototype = {
     draw: function(data) {
         var me = this,
-        rect = this.svg.selectAll().data(data)
-            .enter().append("rect")
-            .style('fill', this.color)
-            .attr("y", me.height - 100)
-            .attr("width", 10)
-            .attr("x", function(d,i){return 10*i})
-            .attr("height", 100),
-        t = rect.transition()
-            .duration(750)
-            .attr("height", function(d) {return d})
-            .attr("y", function(d) {return me.height-d});
-
+        rect = this.svg.append("path").attr("d", me.hline(data))
+            .style('fill','transparent').style('stroke', me.color),
+        t = rect.transition().duration(750).attr('d', me.line(data))
     }
 };
