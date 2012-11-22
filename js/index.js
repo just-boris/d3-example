bcc81c580a1c5121ddcbb26127a650fc56ed8ba3
Boris <just-boris@hotmail.com>
2012-11-22 20:11:55 +0400

    зависимость increment от пользователя

--- js/index.js
+++ js/index.js
@@ -5,35 +5,49 @@ var WIDTH = 500,
     INCREMENT = 10,
     DELTA = 60;
 window.addEventListener('load', function() {
-var
-    subtract = function(a, b) {
-        var result = [];
-        for(var i = 0; i < Math.min(a.length, b.length); i++) {
-            result.push(a[i] - b[i]);
-        }
-        return result;
+var incrementInput = d3.select('#increment');
+    incrementInput.on('change', function() {
+        updateData();
+        redraw();
+    });
+var data1,data2,difference,
+    redraw = function() {
+        g1.draw(data1);
+        g2.draw(data2);
+        g3.draw(difference);
     },
-    data1 = (function getData() {
-        var res = [];
-        for(var i = 0; i < WIDTH/PULSE_WIDTH; i++) {
-            res.push((i*INCREMENT  + DELTA * ((i+1) % 2)) % RESET_VALUE)
-        }
-        return res;
-    })(),
-    data2 = (function shiftData(data) {
-        var res = data.slice();
-        res.unshift(0);
-        res.pop();
-        return res;
-    })(data1),
-    difference = subtract(data1, data2).map(function(i) {return (i+200)%200}),
+    updateData = function() {
+        INCREMENT = incrementInput.node().value;
+        data1 = (function getData() {
+                var res = [];
+                for(var i = 0; i < WIDTH/PULSE_WIDTH; i++) {
+                    res.push((i*INCREMENT  + DELTA * ((i+1) % 2)) % RESET_VALUE)
+                }
+                return res;
+        })();
+        data2 = (function shiftData(data) {
+                var res = data.slice();
+                res.unshift(0);
+                res.pop();
+                return res;
+        })(data1);
+        difference = (function subtract(a, b) {
+            var result = [];
+            for(var i = 0; i < Math.min(a.length, b.length); i++) {
+                result.push(a[i] - b[i]);
+            }
+            return result;
+        })(data1, data2).map(function(i) {return (i+200)%200});
+    },
+
     g1 = new SawGraph('#graph1', 'steelblue'),
     g2 = new SawGraph('#graph2', 'green'),
     g3 = new SawGraph('#graph3', 'red');
-
-    g1.draw(data1);
-    g2.draw(data2);
-    g3.draw(difference);
+    updateData();
+    g1.init(data1);
+    g2.init(data2);
+    g3.init(difference);
+    redraw();
 });
 
 function SawGraph(selector, color) {
@@ -61,9 +75,10 @@ function SawGraph(selector, color) {
 }
 SawGraph.prototype = {
     draw: function(data) {
-        var me = this,
-        rect = this.svg.append("path").attr("d", me.hline(data))
-            .style('fill','transparent').style('stroke', me.color),
-        t = rect.transition().duration(750).attr('d', me.line(data))
+        this.svg.selectAll('path').transition().duration(750).attr('d', this.line(data));
+    },
+    init: function(data) {
+        this.svg.append("path").attr("d", this.hline(data))
+            .style('fill','transparent').style('stroke', this.color)
     }
 };
